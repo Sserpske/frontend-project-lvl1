ec2c1e079c8c6cfd2870a66f1e3ae20e3c1bbff0
Aleksandr <kochura@gmail.com>
2020-02-03 22:06:33 +0300

    CodeClimate, ESLint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { askForName } from '../src/index.js';
+import askForName from '../src/index';
 
 console.log('Welcome to the Brain Games!');
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
-export const askForName = () => {
+const askForName = () => {
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
 };
+
+export default askForName();
